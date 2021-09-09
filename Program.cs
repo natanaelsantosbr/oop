5f93f0a77f6723b6b3e2f4b33a58429cbcd3ca93
Natanael <--replace-all>
2021-09-08 21:16:17 -0300

    Encapsulamento

--- Program.cs
+++ Program.cs
@@ -13,17 +13,30 @@ namespace Payments
 
             Uma classe tem propriedade, metodos, eventos e é um tipo de refencia. Armazena apenas o endereço e não o dado.
 
-            structs é um tipo de valor. Armazenada dados
+            structs é um tipo de valor. Armazenada os dados em si.
 
             Class um molde para um objeto.
+
+            Vantagens de OOP
+            - Pegar um projeto grande e dividi em pequenas partes
             */
 
 
             Console.WriteLine("Hello World!");
         }
 
-        class Customer{
-            string name;
+        /*
+            Encapsulamento
+            Agrupar o que faz sentido esta junto                        
+        */
+        class Pagamento
+        {
+            //Propriedades  (Caractereisticas que o metodo tem)
+            DateTime Vencimento;
+
+            //Metodos (Funções que o metodo tem)
+            void Pagar(){}            
+
         }
     }
 }
