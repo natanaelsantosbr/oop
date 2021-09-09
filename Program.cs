ac3d0f683f6cda1a9c19f263d43cafe8b1058440
Natanael <--replace-all>
2021-09-08 21:10:04 -0300

    Classes e Objeto

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace Payments
     {
         static void Main(string[] args)
         {
+            /*
+            Diferença entre classe e structs ?
+
+            E a forma que é armazenado.
+
+            Uma classe tem propriedade, metodos, eventos e é um tipo de refencia. Armazena apenas o endereço e não o dado.
+
+            structs é um tipo de valor. Armazenada dados
+
+            Class um molde para um objeto.
+            */
+
+
             Console.WriteLine("Hello World!");
+        }
+
+        class Customer{
+            string name;
         }
     }
 }
