fd6a3befc64d47590102756aa0f5661ab69990d2
Natanael <natanaelsantosbr@gmail.com>
2021-09-14 22:26:37 -0300

    Trabalhando com Listas

--- ContentContext/Article.cs
+++ ContentContext/Article.cs
@@ -3,5 +3,6 @@ namespace Balta.ContentContext
     public class Article : Content
     {
 
+
     }
 }
--- ContentContext/Course.cs
+++ ContentContext/Course.cs
@@ -1,7 +1,38 @@
+using System.Collections.Generic;
+
 namespace Balta.ContentContext
 {
     public class Course : Content
     {
+        public Course()
+        {
+            this.Modules = new List<Module>();
+        }
+        public string Tag { get; set; }
 
+        /*Sempre depende da abstração e nunca da implementação . Ex( List)*/
+        public IList<Module> Modules { get; set; }
+    }
+
+    public class Module
+    {
+        public Module()
+        {
+            this.Lectures = new List<Lecture>();
+        }
+
+        public int Order { get; set; }
+
+        public string Title { get; set; }
+
+        public IList<Lecture> Lectures { get; set; }
+    }
+
+    //Aula
+    public class Lecture
+    {
+        public int Order { get; set; }
+
+        public string Title { get; set; }
     }
 }
