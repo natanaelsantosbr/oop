1c30365a38e634a2738b1764cfdbf016bf02c532
Natanael <natanaelsantosbr@gmail.com>
2021-09-16 22:45:33 -0300

    Criando a classe notification

--- ContentContext/Career.cs
+++ ContentContext/Career.cs
@@ -5,6 +5,8 @@ namespace Balta.ContentContext
 {
     public class Career : Content
     {
+
+
         public Career(string title, string url) : base(title, url)
         {
             this.Items = new List<CarrerItem>();
